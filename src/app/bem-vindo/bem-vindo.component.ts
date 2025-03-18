e1262ab86df4d9bbdd62f015b41c528a3e924140
Pedro <pedroferreiralois@gmail.com>
2025-03-18 19:47:41 -0300

    criando a pagina, estilo e componente do bem-vindo

---
+++ src/app/bem-vindo/bem-vindo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bem-vindo',
+  standalone: true,
+  imports: [],
+  templateUrl: './bem-vindo.component.html',
+  styleUrl: './bem-vindo.component.css'
+})
+export class BemVindoComponent {
+
+}
