2703896538c63e83b7461358385dd63878354b48
Pedro <pedroferreiralois@gmail.com>
2025-03-18 19:47:09 -0300

    configurando imports standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppOlaComponent } from './ola/ola.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppOlaComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
-  title = 'frontend-arcevo-musical';
+  title = 'frontend-arcevo-musical2';
+  nome = 'Utfpr!'
 }
