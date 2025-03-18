b3d075a2314cd0d345bb58cc79dd65774549bd41
Pedro <pedroferreiralois@gmail.com>
2025-03-18 19:47:53 -0300

    adicioando ola component

---
+++ src/app/ola/ola.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-ola',
+    templateUrl:'./ola.component.html',
+    styleUrl:'./ola.component.css',
+    standalone:true
+})
+
+
+export class AppOlaComponent{
+    nome = 'Pedro';
+}   
